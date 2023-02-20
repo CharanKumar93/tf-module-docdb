4bd940672b92e648ef2ca6a3caeb196906ff2f69
Charan <gcharankumar99@gmail.com>
2023-02-20 15:02:54 +0530

    vars file

--- main.tf
+++ main.tf
@@ -14,18 +14,18 @@ resource "aws_security_group" "docdb" {
   vpc_id      = var.vpc_id
 
   ingress {
-    description      = "MongoDB"
-    from_port        = 27017
-    to_port          = 27017
-    protocol         = "tcp"
-    cidr_blocks      = var.allow_cidr
+    description = "MongoDB"
+    from_port   = 27017
+    to_port     = 27017
+    protocol    = "tcp"
+    cidr_blocks = var.allow_cidr
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = merge(
@@ -34,13 +34,14 @@ resource "aws_security_group" "docdb" {
   )
 }
 
+
 resource "aws_docdb_cluster" "docdb" {
-  cluster_identifier      = "${var.env}-docdb-cluster"
-  engine                  = "docdb"
-  engine_version          =  var.engine_version
-  master_username         =  data.aws_ssm_parameter.DB_ADMIN_USER.value
-  master_password         =  data.aws_ssm_parameter.DB_ADMIN_PASS.value
-  skip_final_snapshot     =  true
+  cluster_identifier     = "${var.env}-docdb-cluster"
+  engine                 = "docdb"
+  engine_version         = var.engine_version
+  master_username        = data.aws_ssm_parameter.DB_ADMIN_USER.value
+  master_password        = data.aws_ssm_parameter.DB_ADMIN_PASS.value
+  skip_final_snapshot    = true
   db_subnet_group_name   = aws_docdb_subnet_group.default.name
   vpc_security_group_ids = [aws_security_group.docdb.id]
   storage_encrypted      = true
@@ -57,17 +58,11 @@ resource "aws_docdb_cluster_instance" "cluster_instances" {
   identifier         = "${var.env}-docdb-cluster-instance-${count.index + 1}"
   cluster_identifier = aws_docdb_cluster.docdb.id
   instance_class     = var.instance_class
-  storage_encrypted      = true
-  kms_key_id             = data.aws_kms_key.key.arn
+  //  storage_encrypted  = true
+  //  kms_key_id         = data.aws_kms_key.key.arn
 
   tags = merge(
     local.common_tags,
     { Name = "${var.env}-docdb-cluster-instance-${count.index + 1}" }
   )
-
 }
-
-
-
-
-
